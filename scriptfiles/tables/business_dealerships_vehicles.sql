cc86e1b1f3490bbce918699b60d68f43187cfa7a
domingues93 <glederson.r.o@gmail.com>
2022-10-30 01:21:54 -0300

    atualizado sistema de concessionária.

--- scriptfiles/tables/business_dealerships_vehicles.sql
+++ scriptfiles/tables/business_dealerships_vehicles.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `business_dealerships_vehicles`(
-    `id` VARCHAR(64) PRIMARY KEY,
+    `id` CHAR(36) NOT NULL DEFAULT (uuid()) PRIMARY KEY,
     `dealership` INT(11) NOT NULL,
-    `vehicle` VARCHAR(64) NOT NULL UNIQUE,
+    `vehicle` VARCHAR(64) DEFAULT NULL UNIQUE,
+    `model` INT(3) NOT NULL,
     `price` INT(11) NOT NULL,
     `price_sell` INT(11) NOT NULL,
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
