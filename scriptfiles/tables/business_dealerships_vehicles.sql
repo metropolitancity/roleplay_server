c2a60613d8e4a89b5279b373c92df6298ac9a30e
domingues93 <glederson.r.o@gmail.com>
2022-11-10 22:35:16 -0300

    páginação dos veiculos.

--- scriptfiles/tables/business_dealerships_vehicles.sql
+++ scriptfiles/tables/business_dealerships_vehicles.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS `business_dealerships_vehicles`(
     `id` CHAR(36) NOT NULL DEFAULT (uuid()) PRIMARY KEY,
     `dealership` INT(11) NOT NULL,
-    `vehicle` VARCHAR(64) DEFAULT NULL UNIQUE,
     `model` INT(3) NOT NULL,
     `price` INT(11) NOT NULL,
-    `price_sell` INT(11) NOT NULL,
+    `color1` INT(3) NOT NULL,
+    `color2` INT(3) NOT NULL,
     `type` INT(1) NOT NULL,
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`dealership`) REFERENCES `business_dealerships`(`id`),
-    FOREIGN KEY (`vehicle`) REFERENCES `users_vehicles`(`id`)
+    FOREIGN KEY (`dealership`) REFERENCES `business_dealerships`(`id`)
 )ENGINE = INNODB CHARSET="utf8" COLLATE="utf8_general_ci";
